cd71234806ba90f382f0d086d9a71991439096a0
johntymuriithi <muriithijohn634@gmail.com>
2024-07-09 12:21:48 +0300

    flask

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -10,4 +10,24 @@ class BasicAuth(Auth):
     """
     BasicAuth class that inherits from Auth for basic authentication.
     """
-    pass
+
+    def extract_base64_authorization_header(self, authorization_header: str) \
+            -> str:
+        """
+        Extracts the Base64 part of the
+        Authorization header for Basic Authentication.
+
+        Args:
+            authorization_header (str): The Authorization header.
+
+        Returns:
+            str: The Base64 part of the Authorization
+            header, or None if invalid.
+        """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        return authorization_header[len("Basic "):]
