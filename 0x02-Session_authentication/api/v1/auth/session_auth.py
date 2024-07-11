4ee6fd170af23b610054cd930ae3c7eec21e7ab3
johntymuriithi <muriithijohn634@gmail.com>
2024-07-11 22:36:10 +0300

    flask

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,53 +1,83 @@
 #!/usr/bin/env python3
-"""Session Authentication Views"""
-from os import getenv
+
+""" SessionAuth module for API authentication
+"""
+
+from typing import TypeVar
+from api.v1.auth.auth import Auth
+from uuid import uuid4
 from models.user import User
-from api.v1.views import app_views
-from flask import request, jsonify, abort
 
 
-@app_views.route("/auth_session/login", methods=["POST"], strict_slashes=False)
-def login():
-    """POST /auth_session/login
-    Return:
-      - Response
+class SessionAuth(Auth):
+    """ Session authentication class.
     """
-    user_email = request.form.get("email")
-    user_pwd = request.form.get("password")
-    if not user_email:
-        return jsonify(error="email missing"), 400
-    if not user_pwd:
-        return jsonify(error="password missing"), 400
-    try:
-        user = User.search({"email": user_email})
-    except Exception:
-        return jsonify(error="no user found for this email"), 404
-    if not user:
-        return jsonify(error="no user found for this email"), 404
-    for u in user:
-        if u.is_valid_password(user_pwd):
-            user_id = u.id
-            from api.v1.app import auth
+    # initialize dictionary to store user_id by session_id
+    user_id_by_session_id = {}
 
-            session_id = auth.create_session(user_id)
-            response = jsonify(u.to_json())
-            response.set_cookie(getenv("SESSION_NAME"), session_id)
-            return response
-        else:
-            return jsonify(error="wrong password"), 401
-    return jsonify(error="no user found for this email"), 404
+    def create_session(self, user_id: str = None) -> str:
+        """ Creates a session ID for a user_id
+        Args:
+            user_id: The user_id to create a session for
+        Returns:
+            The session ID created
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
 
+        session_id = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
 
-@app_views.route(
-    "/auth_session/logout", methods=["DELETE"], strict_slashes=False
-)
-def logout():
-    """DELETE /auth_session/logout
-    Return:
-      - Response
-    """
-    from api.v1.app import auth
+        return session_id
 
-    if auth.destroy_session(request):
-        return jsonify({}), 200
-    abort(404)
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """ Returns a User ID based on a Session ID
+        Args:
+            session_id: The session ID to look up
+        Returns:
+            The user ID associated with the session ID
+        """
+        if session_id is None or not isinstance(session_id, str):
+            return None
+
+        return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """ Returns a User instance based on a cookie value
+        Args:
+            request: The request object
+        Returns:
+            The user associated with the cookie value
+        """
+        if request is None:
+            return None
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return None
+        user_id = self.user_id_for_session_id(session_id)
+        if user_id is None:
+            return None
+
+        return User.get(user_id)
+
+    def destroy_session(self, request=None):
+        """ Deletes the user session / logout
+        Args:
+            request: The request object
+        Returns:
+            True if session was deleted, False otherwise
+        """
+        if request is None:
+            return False
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return False
+        user_id = self.user_id_for_session_id(session_id)
+        if user_id is None:
+            return False
+        try:
+            del self.user_id_by_session_id[session_id]
+        except Exception:
+            return False
+
+        return True
